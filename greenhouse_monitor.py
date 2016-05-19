35ffec2d52be5a7ffd8249e975bd9cd88f1ce4ea
LarzACarswell <skruffydx@gmail.com>
2016-05-18 20:01:53 -0400

    Add files via upload

--- greenhouse_monitor.py
+++ greenhouse_monitor.py
@@ -4,17 +4,17 @@ import sqlite3
 import RPi.GPIO as gpio
 import time
 from datetime import datetime
-
+import Adafruit_DHT
 
 # global variables (speriod controls the frequency of sensor readings)
-speriod=(15*60)-1
-dbname='/var/www/templog.db'
+speriod=(5)-1
+dbname='/home/pi/Documents/Python_Projects/Greenhouse-Automation-master/templog.db'
 sensor = Adafruit_DHT
 
 
 gpio.setmode(gpio.BCM)
 gpio.setup(4, gpio.IN)
-gpio.setup(14, gpio.OUT)
+#gpio.setup(14, gpio.OUT)
 
 
 # store the temperature in the database
@@ -51,24 +51,24 @@ def display_data():
 # returns None on error, or the temperature as a float
 # modified to write humidity, temperature to csv formatted .txt 
 
-def get_temp(devicefile):
+def get_temp():
 
-    try:
-        dataWrite = open('greenhouse_data.txt', 'a')
-        humidity, temperature = sensor.read_retry(11, 4)
-        now = datetime.now()
-        print now
-        print 'Temp = {0:0.1f}*C Humidity = {1:0.1f}%'.format(temperature, humidity)
-        h = str(humidity)
-        t = str(temperature)
-        dataWrite.write(str(now) + ",")
-        dataDump.write((h) + "," + (t) + "\n")
-        dataWrite.close()
-        tempvalue = float(temperature)/1000
-        temhumid = float(humidity)/1000
-        return tempvalue, humidvalue
-    except:
-        return None
+    dataWrite = open('greenhouse_data.txt', 'a')
+    #dataDump = open('greenhouse_datadump.txt', 'a')
+    humidity, temperature = sensor.read_retry(Adafruit_DHT.DHT11, 4)
+    now = datetime.now()
+    print now
+    print 'Temp = {0:0.1f}*C Humidity = {1:0.1f}%'.format(temperature, humidity)
+    h = str(humidity)
+    t = str(temperature)
+    dataWrite.write(str(now) + ",")
+    #dataDump.write((h) + "," + (t) + "\n")
+    dataWrite.close()
+    tempvalue = float(temperature)/1000
+    temhumid = float(humidity)/1000
+    return tempvalue, temhumid
+    print str(tempvalue)+"!!!!!"
+
 
 
 
@@ -78,32 +78,28 @@ def main():
 
     while True:
 
-    # get the temperature from the device file
-    temperature = get_temp()
-    if temperature != None:
-        print "temperature,humdity="+str(temperature)
-    else:
-        # Sometimes reads fail on the first attempt
-        # so we need to retry
+        # get the temperature from the device file
         temperature = get_temp()
-        print "temperature,humidity="+str(temperature)
+        if temperature != None:
+            print "temperature,humdity="+str(temperature)
+        else:
+            # Sometimes reads fail on the first attempt
+            # so we need to retry
+            temperature = get_temp()
+            print "temperature,humidity="+str(temperature)
 
-        # Store the temperature in the database
-    log_temperature(temperature)
+            # Store the temperature in the database
+        log_temperature(temperature)
 
         # display the contents of the database
         display_data()
-        
+            
         #LED to signal success
-        gpio.output(14, True)
-        sleep(.1)
-        gpio.output(14, False)
+        #gpio.output(14, True)
+        #sleep(.1)
+        #gpio.output(14, False)
         time.sleep(speriod)
 
 
 if __name__=="__main__":
     main()
-
-
-
-
